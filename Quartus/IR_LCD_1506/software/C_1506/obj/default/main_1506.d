797baafcae4c0f471bbc88f6b71cb66a31b23629
Fan JIN <kingium@outlook.com>
2017-08-30 10:36:47 +0800

    FPGA提高实验（基本功能）
    实现了数字输入和删除的基本功能

---
+++ Quartus/IR_LCD_1506/software/C_1506/obj/default/main_1506.d
@@ -0,0 +1,16 @@
+obj/default/main_1506.o: main_1506.c ../C_1506_bsp/system.h \
+ ../C_1506_bsp/linker.h LCD_1506_Qsys.h \
+ ../C_1506_bsp//HAL/inc/sys/alt_irq.h ../C_1506_bsp//HAL/inc/nios2.h \
+ ../C_1506_bsp//HAL/inc/alt_types.h
+
+../C_1506_bsp/system.h:
+
+../C_1506_bsp/linker.h:
+
+LCD_1506_Qsys.h:
+
+../C_1506_bsp//HAL/inc/sys/alt_irq.h:
+
+../C_1506_bsp//HAL/inc/nios2.h:
+
+../C_1506_bsp//HAL/inc/alt_types.h:

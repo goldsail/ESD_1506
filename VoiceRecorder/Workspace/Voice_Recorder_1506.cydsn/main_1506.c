9500175f5dd8e3e5e04832cba30e10d6b5fad222
Fan JIN <kingium@outlook.com>
2017-09-01 11:34:37 +0800

    PSoC提高实验（实时录音上传至电脑）
    尚未实现：从电脑下载录音并播播放

--- VoiceRecorder/Workspace/Voice_Recorder_1506.cydsn/main_1506.c
+++ VoiceRecorder/Workspace/Voice_Recorder_1506.cydsn/main_1506.c
@@ -1,25 +1,92 @@
 
 #include <device.h>
 
-#define THREASHOLD (4)
 
 #define PackageSize (0x40)
-#define buffer_len (0x8000)
-#define PackageNumber (buffer_len / PackageSize)
+#define BufferSize (0x8000)
+#define PackageNumber (BufferSize / PackageSize)
 
-uint8 buffer[buffer_len];
-uint8 buf[PackageSize + 0xF];
+uint8 buffer[BufferSize + 0xF];
+uint8 package[PackageSize + 0xF];
+
+void PrintString(const char8 *str);
+void Init();
 
 
 int main()
 {
     int i;
     int j;
-    register int8 currVoltage;
-    register int8 lastVoltage;
     register int8 Voltage;
-    int count;
     
+    Init();
+
+    while (1) // main loop
+    {
+        if (USBUART_1506_DataIsReady() != 0u)
+        {
+            USBUART_1506_GetAll(package);
+            
+            if (package[0] == 0x00) // start recording
+            {
+                PrintString("Recording");
+                
+                for (i = 0; ; i = (i + 1) % BufferSize)
+                {
+                    if (USBUART_1506_DataIsReady() != 0u)
+                    {
+                        USBUART_1506_GetAll(package);
+                        if (package[0] == 0x01)
+                        {
+                            PrintString("Recorded");
+                            break;
+                        }
+                    }
+                    if (i % PackageSize == 0)
+                    {
+                        while(USBUART_1506_CDCIsReady() == 0u);
+                        USBUART_1506_PutData(package, PackageSize);
+                    }
+                    
+                    ADC_1506_IsEndConversion(ADC_1506_WAIT_FOR_RESULT);
+                    Voltage = ADC_1506_GetResult8();
+                    buffer[i] = Voltage;
+                    package[i % PackageSize] = Voltage;
+                    
+                }
+                
+            }
+            else if (package[0] == 0x02) // replay the current buffer
+            {
+                PrintString("Replaying");
+                for (i = 0; i < BufferSize; i++)
+                {
+                    ADC_1506_IsEndConversion(ADC_1506_WAIT_FOR_RESULT);
+                    ADC_1506_GetResult8();
+                    VDAC_1506_SetValue(buffer[i]);
+                }
+                PrintString("Ready");
+            }
+            else if (package[0] == 0x03)
+            {
+                
+            }
+            
+        }
+    }
+    
+}
+
+void PrintString(const char8 *str)
+{
+    LCD_1506_ClearDisplay();
+    CyDelay(100);
+    LCD_1506_PrintString(str);
+    CyDelay(100);
+}
+
+void Init()
+{
     /* Enable Global Interrupts */
     CyGlobalIntEnable;    
     
@@ -29,82 +96,14 @@ int main()
     VDAC_1506_Start();             
     Opamp_1506_Start();
     USBUART_1506_Start(0u, USBUART_1506_3V_OPERATION);
-    LCD_1506_ClearDisplay();
-    CyDelay(100);
+    
+    PrintString("");
+    
     while (!USBUART_1506_GetConfiguration());
     USBUART_1506_CDC_Init();
 	
     ADC_1506_StartConvert();
     
-    LCD_1506_PrintString("Ready");
-
-    while (1)
-    {
-        if (USBUART_1506_DataIsReady() != 0u)
-        {
-            count = USBUART_1506_GetAll(buf);
-            LCD_1506_PrintNumber(count);
-            
-            if (buf[0] == '0') // start recording
-            {
-                LCD_1506_ClearDisplay();
-                CyDelay(100);
-                LCD_1506_PrintString("Recording");
-                CyDelay(100);
-                
-                for(i = 0; i < buffer_len; i++)
-                {
-                    lastVoltage = currVoltage;
-                    ADC_1506_IsEndConversion(ADC_1506_WAIT_FOR_RESULT);
-                    currVoltage = ADC_1506_GetResult8();
-                    Voltage = currVoltage;
-                    if (Voltage > lastVoltage + THREASHOLD)
-                    {
-                        Voltage = lastVoltage + THREASHOLD;
-                    }
-                    if (Voltage < lastVoltage - THREASHOLD)
-                    {
-                        Voltage = lastVoltage - THREASHOLD;
-                    }
-                    buffer[i] = Voltage;
-                }
-                
-                LCD_1506_ClearDisplay();
-                CyDelay(100);
-                LCD_1506_PrintString("Transferring");
-                CyDelay(100);
-                
-                for (i = 0; i < PackageNumber; i++)
-                {
-                    while(USBUART_1506_CDCIsReady() == 0u);
-                    USBUART_1506_PutData(&buffer[i * PackageSize], PackageSize);
-                }
-                
-                LCD_1506_ClearDisplay();
-                CyDelay(100);
-                LCD_1506_PrintString("Transferred");
-                CyDelay(100);
-            }
-            else
-            {
-                LCD_1506_ClearDisplay();
-                CyDelay(100);
-                LCD_1506_PrintString("Replaying");
-                CyDelay(100);
-                for (i = 0; i < buffer_len; i++)
-                {
-                    ADC_1506_IsEndConversion(ADC_1506_WAIT_FOR_RESULT);
-                    ADC_1506_GetResult8();
-                    VDAC_1506_SetValue(buffer[i]);
-                }
-                LCD_1506_ClearDisplay();
-                CyDelay(100);
-                LCD_1506_PrintString("Ready");
-                CyDelay(100);
-            }
-            
-        }
-    }
-    
+    PrintString("Ready");
 }
 

e4eb66446e43eba0e0aaa9d15f488e1e6c1e71b5
Fan JIN <jinf15@mails.tsinghua.edu.cn>
2017-09-06 22:06:02 +0800

    自平衡车（PID已调参）

---
+++ SelfBalanceScooter/962158.cydsn/Bluetooth.h
@@ -0,0 +1,13 @@
+#ifndef _BLUETOOTH_H
+#define _BLUETOOTH_H
+
+
+#include <project.h>
+
+#define BufferSize (100)
+extern uint8 buffer[BufferSize];
+
+void GetBluetoothString(uint8 *buffer);
+
+
+#endif

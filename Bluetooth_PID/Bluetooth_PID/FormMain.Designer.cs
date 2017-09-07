c49ec3a2713106fbef2e13cf5e86bb259143f589
Fan JIN <jinf15@mails.tsinghua.edu.cn>
2017-09-07 20:52:09 +0800

    远程PID调参（Bluetooth + Kinect）

--- Bluetooth_PID/Bluetooth_PID/FormMain.Designer.cs
+++ Bluetooth_PID/Bluetooth_PID/FormMain.Designer.cs
@@ -28,121 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBoxCom = new System.Windows.Forms.TextBox();
             this.label = new System.Windows.Forms.Label();
             this.buttonConnect = new System.Windows.Forms.Button();
             this.buttonSend = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
+            this.labelJustSent = new System.Windows.Forms.Label();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.buttonMode = new System.Windows.Forms.Button();
+            this.labelMode = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(208, 31);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(213, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "0";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(208, 91);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(213, 23);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "0";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(208, 147);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(213, 23);
-            this.textBox3.TabIndex = 1;
-            this.textBox3.Text = "0";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(208, 211);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(213, 23);
-            this.textBox4.TabIndex = 1;
-            this.textBox4.Text = "8";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(208, 265);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(213, 23);
-            this.textBox5.TabIndex = 1;
-            this.textBox5.Text = "0.09";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(208, 320);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(213, 23);
-            this.textBox6.TabIndex = 1;
-            this.textBox6.Text = "0.48";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(208, 378);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(213, 23);
-            this.textBox7.TabIndex = 1;
-            this.textBox7.Text = "4";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(208, 436);
-            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(213, 23);
-            this.textBox8.TabIndex = 1;
-            this.textBox8.Text = "-10";
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(208, 489);
-            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(213, 23);
-            this.textBox9.TabIndex = 1;
-            this.textBox9.Text = "0";
-            // 
-            // textBox10
-            // 
-            this.textBox10.Location = new System.Drawing.Point(208, 545);
-            this.textBox10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(213, 23);
-            this.textBox10.TabIndex = 1;
-            this.textBox10.Text = "10";
             // 
             // textBoxCom
             // 
@@ -175,10 +71,10 @@
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(493, 450);
+            this.buttonSend.Location = new System.Drawing.Point(478, 262);
             this.buttonSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(320, 118);
+            this.buttonSend.Size = new System.Drawing.Size(217, 59);
             this.buttonSend.TabIndex = 4;
             this.buttonSend.Text = "发送数据";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -187,122 +83,76 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 31);
+            this.label1.Location = new System.Drawing.Point(78, 262);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 17);
+            this.label1.Size = new System.Drawing.Size(99, 17);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Ek";
+            this.label1.Text = "bias (-70 ~ -10)";
             // 
-            // label2
+            // labelJustSent
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 88);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 17);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Ek_1";
+            this.labelJustSent.AutoSize = true;
+            this.labelJustSent.Location = new System.Drawing.Point(141, 495);
+            this.labelJustSent.Name = "labelJustSent";
+            this.labelJustSent.Size = new System.Drawing.Size(108, 17);
+            this.labelJustSent.TabIndex = 6;
+            this.labelJustSent.Text = "MessageJustSent";
             // 
-            // label3
+            // numericUpDown
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 147);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Ek_2";
+            this.numericUpDown.Location = new System.Drawing.Point(239, 262);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            70,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            70,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown.TabIndex = 7;
+            this.numericUpDown.Value = new decimal(new int[] {
+            70,
+            0,
+            0,
+            -2147483648});
             // 
-            // label4
+            // buttonMode
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 211);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(24, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Kp";
+            this.buttonMode.Location = new System.Drawing.Point(81, 63);
+            this.buttonMode.Name = "buttonMode";
+            this.buttonMode.Size = new System.Drawing.Size(149, 103);
+            this.buttonMode.TabIndex = 8;
+            this.buttonMode.Text = "切换模式";
+            this.buttonMode.UseVisualStyleBackColor = true;
+            this.buttonMode.Click += new System.EventHandler(this.buttonMode_Click);
             // 
-            // label5
+            // labelMode
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(47, 265);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(19, 17);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Ki";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(47, 320);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(24, 17);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Kd";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(47, 378);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(132, 17);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "TopMotorAngleCoeff";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(47, 436);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(94, 17);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "ADC_L_Balance";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(47, 489);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(96, 17);
-            this.label9.TabIndex = 6;
-            this.label9.Text = "ADC_R_Balance";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(47, 545);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(42, 17);
-            this.label10.TabIndex = 6;
-            this.label10.Text = "range";
+            this.labelMode.AutoSize = true;
+            this.labelMode.Location = new System.Drawing.Point(267, 153);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(124, 17);
+            this.labelMode.TabIndex = 9;
+            this.labelMode.Text = "当前：Bluetooth模式";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 601);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.buttonMode);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.labelJustSent);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.buttonConnect);
             this.Controls.Add(this.label);
             this.Controls.Add(this.textBoxCom);
-            this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -310,37 +160,22 @@
             this.Name = "FormMain";
             this.Text = "蓝牙远程调参助手";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.TextBox textBoxCom;
         private System.Windows.Forms.Label label;
         private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelJustSent;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button buttonMode;
+        private System.Windows.Forms.Label labelMode;
     }
 }
 
--- Bluetooth_PID/Bluetooth_PID/FormMain.cs
+++ Bluetooth_PID/Bluetooth_PID/FormMain.cs
@@ -8,16 +8,71 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Microsoft.Kinect;
+
+
 namespace Bluetooth_PID
 {
     public partial class FormMain : Form
     {
+        private bool isKinectMode = false;
+
+        private KinectSensor kinectSensor = null;
+
         public FormMain()
         {
             InitializeComponent();
             PortChat.SetPortName(label);
             buttonSend.Enabled = false;
-            
+            buttonMode.Enabled = false;
+
+        }
+
+        public void kinectFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            try
+            {
+                using (BodyFrame bf = e.FrameReference.AcquireFrame())
+                {
+                    if (bf != null)
+                    {
+                        Body[] bodies = new Body[bf.BodyCount];
+                        bf.GetAndRefreshBodyData(bodies);
+                        foreach (Body body in bodies)
+                        {
+                            if (body != null)
+                            {
+                                if (body.IsTracked)
+                                {
+                                    Joint handRight = body.Joints[JointType.WristRight];
+                                    Joint handLeft = body.Joints[JointType.WristLeft];
+
+                                    if (handLeft.Position.Y > handRight.Position.Y)
+                                    {
+
+                                        int temp = (int)((handRight.Position.Z - 0.8) * 200 - 40);
+
+                                        temp = Math.Min(Math.Max(temp, -70), -10);
+
+                                        string str = $" {temp}";
+
+                                        PortChat.SendRawString(str);
+
+                                        labelJustSent.Text = str;
+
+                                        break;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (PortChat.PortDisconnectException)
+            {
+                Console.WriteLine("serial port disconnected");
+            }
         }
 
         private void buttonConnect_Click(object sender, EventArgs e)
@@ -26,6 +81,7 @@ namespace Bluetooth_PID
             {
                 PortChat.Initialize(textBoxCom.Text);
                 buttonSend.Enabled = true;
+                buttonMode.Enabled = true;
                 buttonConnect.Enabled = false;
             }
             catch (Exception)
@@ -36,97 +92,20 @@ namespace Bluetooth_PID
 
         private void buttonSend_Click(object sender, EventArgs e)
         {
-            string[] paras = new string[]
-            {
-                textBox1.Text,
-                textBox2.Text,
-                textBox3.Text,
-                textBox4.Text,
-                textBox5.Text,
-                textBox6.Text,
-                textBox7.Text,
-                textBox8.Text,
-                textBox9.Text,
-                textBox10.Text,
-            };
-
-            float tempF = 0;
-            int tempD = 0;
+            int val = (int)numericUpDown.Value;
             
-            if (!float.TryParse(paras[0], out tempF))
+            if (val < -200 || val > 200)
             {
                 MessageBox.Show("参数不合法");
                 return;
             }
 
-            if (!float.TryParse(paras[1], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[2], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[3], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[4], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[5], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[6], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!int.TryParse(paras[7], out tempD))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!int.TryParse(paras[8], out tempD))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            if (!float.TryParse(paras[9], out tempF))
-            {
-                MessageBox.Show("参数不合法");
-                return;
-            }
-
-            string ret = "";
-
-            for (int i =0; i < 10; i++)
-            {
-                if (i > 0)
-                {
-                    ret += " ";
-                }
-                ret += paras[i];
-            }
+            string ret = " " + val.ToString();
 
             try
             {
                 PortChat.SendRawString(ret);
+                labelJustSent.Text = ret;
             }
             catch (Exception)
             {
@@ -137,6 +116,26 @@ namespace Bluetooth_PID
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonMode_Click(object sender, EventArgs e)
+        {
+            if (isKinectMode)
+            {
+                buttonSend.Enabled = true;
+                isKinectMode = false;
+                kinectSensor.BodyFrameSource.OpenReader().FrameArrived -= kinectFrameReader_FrameArrived;
+                labelMode.Text = "当前：Bluetooth模式";
+            }
+            else
+            {
+                buttonSend.Enabled = false;
+                isKinectMode = true;
+                kinectSensor = KinectSensor.GetDefault();
+                kinectSensor.BodyFrameSource.OpenReader().FrameArrived += kinectFrameReader_FrameArrived;
+                labelMode.Text = "当前：Kinect模式";
+                kinectSensor.Open();
+            }
         }
     }
 }
